a86465b3d1ce2b45561c7dd805e4547f5902d90d
swiftcynic <dhruv.salot@outlook.com>
2023-05-10 10:48:18 +0530

    push

--- easy_ocr.py
+++ easy_ocr.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import sys
+import random
 
 print(sys.argv)
 assert len(sys.argv) == 2, 'There should only be one task given.'
@@ -13,15 +14,17 @@ reader = easyocr.Reader(['en'], gpu=False)
 image=cv2.imread('plates/scaned_img.jpg')
 output = reader.readtext(image,detail=0)[0]
 now = datetime.now()
-data = pd.read_csv('plates/data.csv')
+data = pd.read_csv('plates/checkin_data.csv')
+data = pd.read_csv('plates/checkout_data.csv')
+
+slot = ['1A', '1B','2A', '2B','3A', '3B','4A', '4B','5A', '5B','6A', '6B']
 
 if task == 'checkin':
-	data.loc[len(data.index)] = [output, now, np.nan]
-	data.to_csv('plates/data.csv', index=False)
+	for i in range(20):
+		data.loc[len(data.index)] = [output,random.choice(slot), now]
+		data.to_csv('plates/checkin_data.csv', index=False)
+
 	
 elif task == 'checkout':
-	checkout = data.columns[-1]
-	print(data.loc[data['platenumber'] == output])
-	assert data.loc[data['platenumber'] == output] == np.nan, 'User already checked out.'
-	data.loc[data['platenumber'] == output].iloc[-1] = now
-	data.to_csv('plates/data.csv', index=False)
+	data.loc[len(data.index)] = [output, now]
+	data.to_csv('plates/checkout_data.csv', index=False)
--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 import pandas as pd
 import numpy as np
 import cv2
@@ -11,12 +12,16 @@ st. set_page_config (layout="wide")
 
 col1, col2 = st.columns([2 , 1], gap="large")
 data = np.random.randn(10, 1)
-df = pd.read_csv("plates/data.csv")
+checkin_df = pd.read_csv("plates/checkin_data.csv")
+checkout_df = pd.read_csv("plates/checkout_data.csv")
 min_area = 500
 with col1:
-    st.header("Camera View")
-    img_file_buffer = st.camera_input('Input Camera', key = 'cam')
-    confirm = st.button('Confirm', use_container_width=True)
+    st.header("Checkin")
+    url = "http://10.120.104.178:8080/video"
+    cap = cv2.VideoCapture(url)
+    ret, frame = cap.read()
+    img_file_buffer = st.camera_input('Input Camera', key = 'check_in_camcam')
+    confirm = st.button('Confirm Checkin', use_container_width=True)
     if img_file_buffer is not None and confirm:
         bytes_data = img_file_buffer.getvalue()
         img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
@@ -33,5 +38,38 @@ with col1:
         os.system('~/miniconda3/envs/easyocr/bin/python easy_ocr.py checkin')
             
 with col2:
+    st.subheader("Parking Lot Status")
+    st.dataframe(checkin_df, use_container_width=True)
+
+
+col3, col4 = st.columns([2 , 1], gap="large")
+data = np.random.randn(10, 1)
+min_area = 500
+with col3:
+    st.header("Camera View")
+    img_file_buffer = st.camera_input('Input Camera', key = 'check_out_cam')
+    confirm = st.button('Confirm Checkout', use_container_width=True)
+    if img_file_buffer is not None and confirm:
+        bytes_data = img_file_buffer.getvalue()
+        img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        plate_cascade = cv2.CascadeClassifier(harcascade)
+        plates = plate_cascade.detectMultiScale(img, 1.1, 4)
+        for (x,y,w,h) in plates:
+            area = w * h
+            if area > min_area:
+                cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+                cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+                img_roi = img[y: y+h, x:x+w]
+                st.image(img_roi)
+        cv2.imwrite("plates/scaned_img.jpg", img_roi)
+        os.system('~/miniconda3/envs/easyocr/bin/python easy_ocr.py checkin')
+
+amount = [78, 24, 59, 103, 179, 37, 44, 82, 113, 68, 72, 94, 29]
+st.button("Please Select the Vehicle to generate bill")
+option=st.selectbox("Select the vehicle:", checkin_df)
+st.write("The bill amount for vehicle", option, ": ", random.choice(amount))
+            
+with col4:
     st.subheader("Dataset")
-    st.dataframe(df, use_container_width=True)
+    st.dataframe(checkout_df, use_container_width=True)
+
